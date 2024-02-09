102c81b45cfe3c9dc7e37ebb6a18829372468c74
Rishabh1330 <159310106+Rishabh1330@users.noreply.github.com>
2024-02-09 14:24:57 +0530

    calc.sol

--- calc.sol
+++ calc.sol
@@ -2,7 +2,7 @@
 // compiler version must be greater than or equal to 0.8.20 and less than 0.9.0
 pragma solidity ^0.8.20;
 
-contract HelloWorld {
+contract calc {
     int i=10;
     uint j=120;
     function display(int i) public view returns(int){
